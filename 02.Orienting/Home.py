72285e90b00208335c7e730b09538b0ba6374505
mariana.steffens@siemens.com <mariana.steffens@siemens.com>
2025-01-25 19:29:31 +0100

    removing git icon

--- 02.Orienting/Home.py
+++ 02.Orienting/Home.py
@@ -31,7 +31,7 @@ logger = logging.getLogger("streamlit")  # Use Streamlit's logger name
 st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .css-1jc7ptx, .e3i9eg81, .viewerBadge_container__1QSob,
     .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
     .viewerBadge_text__1JaDK {
         display: none;
